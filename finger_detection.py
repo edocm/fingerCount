260d925c0cdf1d0bf5111a9f7f3245e209621967
edocm <edocm@mailbox.org>
2021-06-22 16:26:09 +0200

    finger counter complete -> ready for presentation

--- finger_detection.py
+++ finger_detection.py
@@ -30,8 +30,6 @@ def draw_rect(frame):
     for i in range(total_rectangle):
         cv.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]), (hand_rect_two_y[i], hand_rect_two_x[i]), (0, 255, 0), 1)
 
-    return frame
-
 
 def hand_histogram(frame):
     global hand_rect_one_x, hand_rect_one_y, total_rectangle
@@ -46,33 +44,68 @@ def hand_histogram(frame):
     return cv.normalize(hand_hist, hand_hist, 0, 255, cv.NORM_MINMAX)  # Histogramm wird normalisiert
 
 
-def hist_masking(frame, hist):
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+def masking(frame_face, hist):
+    hsv = cv.cvtColor(frame_face, cv.COLOR_BGR2HSV)
     mask = cv.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)  # noch sehr verrauscht
 
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (31, 31))
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (37, 37))
     mask = cv.filter2D(mask, -1, kernel)  # Filtern um rauschen zu entfernen
 
-    _, mask = cv.threshold(mask, 100, 255, cv.THRESH_BINARY)  # Werte der Maske werden entweder 0 oder 255 -> treshold: Entscheidungsschwelle bis zu welchem Wert werden Values 0 gesetzt
+    kernel_erode = cv.getStructuringElement(cv.MORPH_ELLIPSE, (40, 40))
+    kernel_dilate = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10, 10))
+    mask = cv.erode(mask, kernel_erode)
+    mask = cv.dilate(mask, kernel_dilate)
 
+    _, mask = cv.threshold(mask, 50, 255, cv.THRESH_BINARY)  # Werte der Maske werden entweder 0 oder 255 -> treshold: Entscheidungsschwelle bis zu welchem Wert werden Values 0 gesetzt
+
+    return mask
+
+
+def finger_detection(frame_bg, frame, hand_hist):
+    mask = masking(frame_bg, hand_hist)
     mask = cv.merge((mask, mask, mask))  # wir haben 3 Farbkomponenten
-
-    return cv.bitwise_and(frame, mask)  # Bitweise AND-Verknüpfung des Originalbildes mit der Maske
-
-
-def finger_detection(frame, hand_hist):
-    hist_mask_image = hist_masking(frame, hand_hist)
+    hist_mask_image = cv.bitwise_and(face_detection(frame_bg), mask)
 
     # größte Kontur finden und anzeigen
     contour_list = contours(hist_mask_image)
     max_cont = max(contour_list, key=cv.contourArea)
-    cv.drawContours(frame, max_cont, -1, 1)
+    cv.drawContours(frame_bg, max_cont, -1, 1)
 
     # Mittelpunkt der Kontur finden und anzeigen
     cont_centroid = centroid(max_cont)
-    cv.circle(frame, cont_centroid, 5, [255, 0, 255], -1)
+    cv.circle(frame_bg, cont_centroid, 5, [255, 0, 255], -1)
 
-    return frame
+    if max_cont is not None:
+        hull = cv.convexHull(max_cont, returnPoints=False)
+        draw_hull = cv.convexHull(max_cont)
+        defects = cv.convexityDefects(max_cont, hull)
+        cv.drawContours(frame_bg, [draw_hull], 0, [0, 0, 255], 1)
+
+        points = []
+        dist = lambda p1, p2: (np.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2))
+
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(max_cont[s][0])
+            #end = tuple(max_cont[e][0])
+            #far = tuple(max_cont[f][0])
+            if d/256 > 9:  # nur Punkte mit ensprechender defect tiefe
+                points.append(start)
+
+        finger_tips = []
+        _, y_rect, b_rect , h_rect = cv.boundingRect(max_cont)
+        for i in range(len(points)-1):  # durch alle Punkte iterieren
+            if dist(points[i], points[i+1]) > 45:  # distanz zwischen zwei Punkten muss über 25 sein
+                if dist(points[i], cont_centroid) > 0.40*((h_rect+b_rect)/2):  # distanz zwischen Punkt und Handmitte muss über 40% der Höhe der Bounding Box entsprechen
+                    if points[i][1] < y_rect+0.8*h_rect:  # Punkte die an der Unterseite der Handliegen werden nicht eingeblendet
+                        if len(finger_tips) <= 4:  # maximal 5 finger
+                            finger_tips.append(points[i])
+
+        for finger in finger_tips:
+            cv.circle(frame, finger, 5, [0, 0, 255], -1)
+            cv.circle(frame_bg, finger, 5, [0, 0, 255], -1)
+
+        cv.putText(frame, str(len(finger_tips)), cont_centroid, cv.FONT_HERSHEY_PLAIN, 10, [255, 0, 0], 2)
 
 
 def contours(hist_mask_image):
@@ -94,25 +127,56 @@ def centroid(max_contour):
         return None
 
 
+def face_detection(frame):
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(frame_gray)
+    for x, y, w, h in faces:
+        cv.ellipse(frame, (int(x+0.5*w), int(y+0.5*h)), (int(w*0.75), h), 0, 0, 360, 0, -1)
+    return frame
+
+
+def remove_background(frame, bg):
+    threshold = 10
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    bg = cv.cvtColor(bg, cv.COLOR_BGR2GRAY)
+    rows, cols = frame_gray.shape
+    for i in range(rows):
+        for j in range(cols):
+            if frame_gray[i, j] >= bg[i, j] - threshold and frame_gray[i, j] <= bg[i, j] + threshold:
+                frame_gray[i, j] = 0
+            else:
+                frame_gray[i, j] = 255
+    mask = cv.merge((frame_gray, frame_gray, frame_gray))
+    frame = cv.bitwise_and(frame, mask)
+    return frame
+
+
 def main():
     is_hand_hist_created = False
     capture = cv.VideoCapture(0)
 
     while capture.isOpened():
-        _, frame = capture.read()
+        _, frame_bg = capture.read()  # background frame
+        _, frame = capture.read() # frame
         pressed_key = cv.waitKey(5)
+
+        if pressed_key & 0xFF == ord('b'):
+            _, bg = capture.read()
 
         if pressed_key & 0xFF == ord('c'):
             hand_hist = hand_histogram(frame)
             is_hand_hist_created = True
 
         if is_hand_hist_created:
-            frame = finger_detection(frame, hand_hist)
+            #frame = remove_background(frame, bg)
+            finger_detection(frame_bg, frame, hand_hist)
 
         else:
-            frame = draw_rect(frame)
+            draw_rect(frame)
 
         cv.imshow('webcam', frame)
+        cv.imshow('background', frame_bg)
 
         if pressed_key & 0xFF == ord('d'):
             break
